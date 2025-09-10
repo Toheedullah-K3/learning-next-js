f9195a4b7b5756cd1080de4a52fdc32a3a57bbce
Toheedullah-K3 <toheedullah002@gmail.com>
2025-09-10 17:29:00 +0500

    generateMetadata

--- routing-demo/src/app/products/[productId]/page.tsx
+++ routing-demo/src/app/products/[productId]/page.tsx
@@ -1,8 +1,20 @@
+import { Metadata } from "next"
+
+type Props = {
+    params: Promise<{productId: string}>
+}
+
+export const generateMetadata = async ({ params } : Props) : Promise<Metadata> => {
+    const id = (await params).productId
+    return {
+        title: `Product ${id}`
+    }
+}
+
+
 export default async function ProductDetails({
     params,
-}: {
-    params: Promise<{ productId: string }>
-}){
+}: Props){
 
     const id = (await params).productId
     return (
