eabf4b1c40e7e8c85c6918ba3e7ad4a4ad841aaf
Toheedullah-K3 <toheedullah002@gmail.com>
2025-09-16 15:19:47 +0500

    next/navigation - useRouter.push

--- routing-demo/src/app/products/page.tsx
+++ routing-demo/src/app/products/page.tsx
@@ -1,17 +1,30 @@
-import Link from "next/link"
+"use client"
 
-export default function ProductList(){
+import Link from "next/link"
+import { useRouter } from "next/navigation"
+
+export default function ProductList() {
+    const router = useRouter()
 
     const productLinks = [
-        {name: 'Product 1', slug: '/products/1'}, 
-        {name: 'Product 2', slug: '/products/2'}, 
-        {name: 'Product 3', slug: '/products/3'}, 
+        { name: 'Product 1', slug: '/products/1' },
+        { name: 'Product 2', slug: '/products/2' },
+        { name: 'Product 3', slug: '/products/3' },
     ]
+
+    const handleClicled = () => {
+        router.push('/')
+    }
     return (
         <div>
             {
                 productLinks.map(product => (
-                    <Link 
+                    <Link
+                        style={{
+                            fontSize: 22,
+                            color: "black",
+                            marginRight: 20
+                        }}
                         href={product.slug}
                         key={product.slug}
                     >
@@ -19,6 +32,8 @@ export default function ProductList(){
                     </Link>
                 ))
             }
+
+            <button onClick={handleClicled}> Place Order </button>
         </div>
-    ) 
+    )
 }
